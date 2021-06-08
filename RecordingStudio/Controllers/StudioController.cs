84760960ffd99c1cabb0b9a5946898d73a052579
Alexander <nzrchnsk@gmail.com>
2021-06-08 15:35:10 +0300

    Anonymous method in studioController

--- RecordingStudio/Controllers/StudioController.cs
+++ RecordingStudio/Controllers/StudioController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RecordingStudio.Models;
@@ -19,6 +20,7 @@ namespace RecordingStudio.Controllers
         }
 
         // GET: api/Studio
+        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Studio>>> GetStudios()
         {
